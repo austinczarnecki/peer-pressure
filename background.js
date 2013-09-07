d6e3bc5e917278ebbb2842ac54ac4c10f1c9c20d
hengchu <hengchu.zhang@gmail.com>
2013-09-07 17:42:44 -0400

    added punish user function

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+var token = "CAACEdEose0cBAExBljIifV3mGWrDZBpHhToojlmVfi3MEIHnLGUEngv5M0sYCbcwqgjmgu0fEgvWmAEr9Ejy7FZC3LiFAx7zhovQpn4WYEhueJeqWNp3uTPKPmeOgZAU5ZBytwBR4FmwD7n2lF0ufstZCRkkNC4gg08ZChwB29yq8ntMFhgRhG5YDKljhWAQAZD"
+
 localStorage.clear();
 
 var now = new Date();
@@ -44,25 +46,25 @@ parseUri.options = {
 };
 //end parse uri code
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  console.log(tab);
-  console.log(localStorage);
-  console.log('tabs.onUpdated --'
-              + ' window: ' + tab.windowId
-              + ' tab: '    + tab.id
-              + ' index: '  + tab.index
-              + ' url: '    + tab.url);
-  console.log(parseUri(tab.url).host);
-  var visited = parseUri(tab.url).host
-  var keysArray = Object.keys(localStorage);
-  console.log(visited);
-  console.log(keysArray);
-  if (keysArray.indexOf(visited) != -1) {
-    chrome.tabs.remove(tab.id);
-  }
-});
+// function to load another js file
+function loadScript(url, callback)
+{
+    // adding the script tag to the head as suggested before
+   var head = document.getElementsByTagName('head')[0];
+   var script = document.createElement('script');
+   script.type = 'text/javascript';
+   script.src = url;
 
-chrome.tabs.onCreated.addListener(function(tab) {
+   // then bind the event to the callback function 
+   // there are several events for cross browser compatibility
+   script.onreadystatechange = callback;
+   script.onload = callback;
+
+   // fire the loading
+   head.appendChild(script);
+}
+
+function punishUser(tab) {
   console.log(tab);
   console.log(localStorage);
   console.log('tabs.onUpdated --'
@@ -76,6 +78,23 @@ chrome.tabs.onCreated.addListener(function(tab) {
   console.log(visited);
   console.log(keysArray);
   if (keysArray.indexOf(visited) != -1) {
-    chrome.tabs.remove(tab.id);
+    if (!tab["postedToFB"]) {
+      $.post('https://graph.facebook.com/' + '100006458279825' + '/feed', { access_token: token, message: "I am visiting " + tab.url + " again!" }, function(response, status, request) {
+        console.log(request.getAllResponseHeaders());
+        tab["postedToFB"] = true;
+      });
+    }
+    // chrome.tabs.remove(tab.id);
   }
+}
+
+loadScript('jquery.min.js', function () {
+  // now we have jquery enabled yay
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    punishUser(tab);
+  });
+
+  // chrome.tabs.onCreated.addListener(function(tab) {
+  //   punishUser(tab);
+  // });
 });
