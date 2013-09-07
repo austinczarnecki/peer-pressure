fa72bc5221bfbbd050c68253e278b12d799e04d0
Austin Czarnecki <austin.czarnecki@gmail.com>
2013-09-07 15:58:24 -0400

    jquery stuff

--- options.js
+++ options.js
@@ -1,12 +1,21 @@
 // Save this script as `options.js`
 
 //load current blocked sites into table
-$
+function reload_table() {
+  $('#blocked-sites').empty();
+  var keysArray = Object.keys(localStorage);
+  for (var i=0;i<keysArray.length;i++) {
+    $('#blocked-sites').append('<tr><td class="site">'+keysArray[i]+'</td></tr>');
+  }
+}
 
 // Saves options to localStorage.
 function save_options() {
   var url = document.getElementById("url").value;
   localStorage[url] = true;
+
+  //update the table
+  reload_table();
 
   // Update status to let user know options were saved.
   var status = document.getElementById("status");
@@ -31,5 +40,7 @@ function restore_options() {
     }
   }
 }
+
+$(document).ready(reload_table);
 document.addEventListener('DOMContentLoaded', restore_options);
 document.querySelector('#save').addEventListener('click', save_options);
