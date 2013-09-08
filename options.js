86fe32ba5c653deed13340794efe51ba6ec3b024
Austin Czarnecki <austin.czarnecki@gmail.com>
2013-09-07 21:03:04 -0400

    fixed form submission

--- options.js
+++ options.js
@@ -5,7 +5,9 @@ function reload_table() {
   $('#blocked-sites').empty();
   var keysArray = Object.keys(localStorage);
   for (var i=0;i<keysArray.length;i++) {
-    $('#blocked-sites').append('<tr><td class="delete">x</td><td class="site">'+keysArray[i]+'</td></tr>');
+    if (keysArray[i] != "userToken" && keysArray[i] != "userId") {
+      $('#blocked-sites').append('<tr><td class="delete">x</td><td class="site">'+keysArray[i]+'</td></tr>');
+    }
   }
 }
 
@@ -21,33 +23,12 @@ function append_site(site) {
 // Saves options to localStorage.
 function save_options() {
   var url = document.getElementById("url").value;
+  console.log(url);
+  $('#url').val("");
   localStorage[url] = true;
 
   //update the table
   append_site(url);
-
-  // Update status to let user know options were saved.
-  var status = document.getElementById("status");
-  status.innerHTML = "Saving site to blacklist.";
-  setTimeout(function() {
-    status.innerHTML = "";
-  }, 750);
-}
-
-// Restores select box state to saved value from localStorage.
-function restore_options() {
-  var favorite = localStorage["favorite_color"];
-  if (!favorite) {
-    return;
-  }
-  var select = document.getElementById("color");
-  for (var i = 0; i < select.children.length; i++) {
-    var child = select.children[i];
-    if (child.value == favorite) {
-      child.selected = "true";
-      break;
-    }
-  }
 }
 
 $(document).ready(function() {
@@ -58,11 +39,14 @@ $(document).ready(function() {
   });
 });
 
-document.addEventListener('DOMContentLoaded', restore_options);
-$('form').submit(function() {
+$('#input-site').submit(function() {
+  console.log("test");
+  save_options();
+})
+
+$('#user-auth').submit(function() {
   var userId = document.getElementById("userId").value;
   var userToken = document.getElementById("token").value;
   localStorage["userId"] = userId;
   localStorage["userToken"] = userToken;
 })
-document.querySelector('#save').addEventListener('click', save_options);
